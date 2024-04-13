57453f100078baa15ec4bcd53e233d8738e6cfb9
adm9835 <andy_moody_7@yahoo.com>
2024-04-13 19:21:23 -0400

    add music

--- main.gd
+++ main.gd
@@ -2,11 +2,23 @@ extends Node
 
 @onready var battle_scene = preload("res://level/battle.tscn")
 @onready var bgm_player: AudioStreamPlayer = $BGMPlayer
+@onready var escape_menu: Control = $EscapeMenu
+@onready var level: Node3D = $Level
+@onready var options_menu: Control = $"EscapeMenu/%OptionsMenu"
 
 
-func _input(event):
-	if event.is_action_pressed("ui_cancel"):
-		get_tree().quit()
+func _ready():
+	options_menu.resume_pressed.connect(_on_resume_pressed)
+
+
+func _input(_event):
+	if Input.is_action_just_pressed("ui_cancel"):
+		if escape_menu.visible:
+			level.process_mode = Node.PROCESS_MODE_INHERIT
+			escape_menu.hide()
+		else:
+			level.process_mode = Node.PROCESS_MODE_DISABLED
+			escape_menu.show()
 
 
 func start_battle(player_mon: PackedScene, wild_mon: PackedScene):
@@ -20,3 +32,8 @@ func start_battle(player_mon: PackedScene, wild_mon: PackedScene):
 
 func end_battle():
 	bgm_player.play()
+
+
+func _on_resume_pressed():
+	level.process_mode = Node.PROCESS_MODE_INHERIT
+	escape_menu.hide()
---
+++ ui/OptionsMenu.gd
@@ -0,0 +1,38 @@
+extends Control
+signal resume_pressed
+@onready var resume_button: Button = %ResumeButton
+@onready var exit_button: Button = %ExitButton
+@onready var master_slider: HSlider = %MasterSlider
+@onready var sfx_slider: HSlider = %SFXSlider
+@onready var bgm_slider: HSlider = %BGMSlider
+
+
+func _ready():
+	master_slider.value_changed.connect(_on_volume_changed)
+	sfx_slider.value_changed.connect(_on_volume_changed)
+	bgm_slider.value_changed.connect(_on_volume_changed)
+	resume_button.pressed.connect(_on_resume_pressed)
+	exit_button.pressed.connect(_on_exit_pressed)
+
+
+func _on_resume_pressed():
+	emit_signal("resume_pressed")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+
+func _on_volume_changed(_value):
+	if master_slider.value <= -20.0:
+		master_slider.value = -80.0
+	else:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), master_slider.value)
+	if sfx_slider.value <= -20.0:
+		sfx_slider.value = -80.0
+	else:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), sfx_slider.value)
+	if bgm_slider.value <= -20.0:
+		bgm_slider.value = -80.0
+	else:
+		AudioServer.set_bus_volume_db(AudioServer.get_bus_index("BGM"), bgm_slider.value)
