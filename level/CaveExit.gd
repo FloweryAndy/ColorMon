3ab2edd009da5c59da6fba842cfb2a61d403270b
adm9835 <andy_moody_7@yahoo.com>
2024-04-14 19:11:35 -0400

    add cave

---
+++ level/CaveExit.gd
@@ -0,0 +1,15 @@
+extends Area3D
+
+@onready var current_level: Node3D = $".."
+@onready var cave: PackedScene = preload("res://level/overworld.tscn")
+
+
+func _ready():
+	connect("body_entered", _on_body_entered)
+
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		var new_level = cave.instantiate()
+		current_level.get_parent().add_child(new_level)
+		current_level.queue_free()
