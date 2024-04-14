912bf63d61c12d9a26f60f2a8aab2e3138b2ed30
adm9835 <andy_moody_7@yahoo.com>
2024-04-14 19:06:12 -0400

    Implement Game Start

--- main.gd
+++ main.gd
@@ -1,26 +1,34 @@
 extends Node
 
+var game_started: bool = false
 @onready var battle_scene = preload("res://level/battle.tscn")
-@onready var bgm_player: AudioStreamPlayer = $BGMPlayer
+@onready var game_start_bgm_player: AudioStreamPlayer = $GameStartBGMPlayer
+@onready var overworld_bgm_player: AudioStreamPlayer = $OverworldBGMPlayer
 @onready var escape_menu: Control = $EscapeMenu
 @onready var level: Node3D = $Level
 @onready var options_menu: Control = $"EscapeMenu/%OptionsMenu"
 @onready var menu_sound: AudioStreamPlayer = $MenuSound
+@onready var game_start: Control = $GameStart
 
 
 func _ready():
 	options_menu.resume_pressed.connect(_on_resume_pressed)
+	game_start.start_game.connect(_on_game_start)
+	game_start_bgm_player.play()
+	if !game_started:
+		disable_process()
 
 
 func _input(_event):
-	if Input.is_action_just_pressed("ui_cancel"):
+	if Input.is_action_just_pressed("escape"):
+		menu_sound.play()
 		if escape_menu.visible:
-			menu_sound.play()
-			enable_process()
+			if game_started:
+				enable_process()
 			escape_menu.hide()
 		else:
-			menu_sound.play()
-			disable_process()
+			if game_started:
+				disable_process()
 			escape_menu.show()
 
 
@@ -33,7 +41,7 @@ func start_battle(player_mon: PackedScene, wild_mon: PackedScene) -> Node3D:
 	battle.global_position = Vector3(0, -10, 0)
 	battle.battle_finished.connect(end_battle)
 	call_deferred("disable_process")
-	bgm_player.stop()
+	overworld_bgm_player.stop()
 	return battle
 
 
@@ -42,7 +50,7 @@ func end_battle():
 	await get_tree().process_frame
 	var player = get_tree().get_first_node_in_group("player")
 	player.camera.current = true
-	bgm_player.play()
+	overworld_bgm_player.play()
 
 
 func enable_process():
@@ -56,3 +64,9 @@ func disable_process():
 func _on_resume_pressed():
 	enable_process()
 	escape_menu.hide()
+
+
+func _on_game_start():
+	overworld_bgm_player.play()
+	game_started = true
+	enable_process()
