f9649e9abaf261253a394e271ce885a15b6a3f6a
adm9835 <andy_moody_7@yahoo.com>
2024-05-20 22:36:34 -0400

    refactor interactable

--- component/interactable.gd
+++ component/interactable.gd
@@ -1,5 +1,4 @@
 extends Node
-
 signal dialogue_finished
 @export var dialogue: DialogueResource = null
 var started_with_dialogue: bool = false
@@ -12,33 +11,33 @@ func _ready() -> void:
 	await get_tree().process_frame
 	dialogue_box = get_tree().get_root().get_node("Main/DialogueBox")
 	dialogue_label = dialogue_box.dialogue_label
-
 	if dialogue:
 		started_with_dialogue = true
 
 
 func interact() -> bool:
 	menu_sound.play()
-	if dialogue and dialogue_box.visible == false:
-		dialogue_box.show()
-		var dialogue_line = await dialogue.get_next_dialogue_line("this_is_a_node_title")
-		dialogue_label.dialogue_line = dialogue_line
-		dialogue_label.type_out()
-		return true
-	elif dialogue and dialogue_box.visible == true:
-		if dialogue_label.is_typing:
-			dialogue_label.skip_typing()
+	if dialogue:
+		if !dialogue_box.visible:
+			dialogue_box.show()
+			var dialogue_line = await dialogue.get_next_dialogue_line("this_is_a_node_title")
+			dialogue_label.dialogue_line = dialogue_line
+			dialogue_label.type_out()
+			return true
 		else:
-			var next_id: String = dialogue_label.dialogue_line.next_id
-			var dialogue_line: DialogueLine = await dialogue.get_next_dialogue_line(next_id)
-			if dialogue_line:
-				dialogue_label.dialogue_line = dialogue_line
-				dialogue_label.type_out()
+			if dialogue_label.is_typing:
+				dialogue_label.skip_typing()
 			else:
-				emit_signal("dialogue_finished")
-				dialogue_box.hide()
-				if !started_with_dialogue:
-					dialogue = null
-				return false
-		return true
+				var next_id: String = dialogue_label.dialogue_line.next_id
+				var dialogue_line: DialogueLine = await dialogue.get_next_dialogue_line(next_id)
+				if dialogue_line:
+					dialogue_label.dialogue_line = dialogue_line
+					dialogue_label.type_out()
+				else:
+					emit_signal("dialogue_finished")
+					dialogue_box.hide()
+					if !started_with_dialogue:
+						dialogue = null
+					return false
+			return true
 	return false
